d9d81203944d610ddfb1d8f4920c8ce307639057
Brent Hankins <a.brenthankins@gmail.com>
2013-07-11 02:15:37 +0000

    web.js try 2

--- web.js
+++ web.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
     blarg = fs.readFileSync(index.html,utf8);
     response.send(blarg);
 });
