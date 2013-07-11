c7b7e901cbdeedc40f1607a53182385ed4de9f25
Brent Hankins <a.brenthankins@gmail.com>
2013-07-11 02:22:44 +0000

    attempt 4

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var blarg = fs.readFileSync(index.html,utf8);
+    var blarg = fs.readFileSync('./index.html','utf8');
     response.send(blarg);
 });
 
