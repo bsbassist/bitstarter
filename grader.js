6b25a3ae30390770cabb9c35a97aa6118c9044c5
Brent Hankins <a.brenthankins@gmail.com>
2013-07-12 03:06:54 +0000

    added program.url option

--- grader.js
+++ grader.js
@@ -67,6 +67,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'Path to URL', clone(assertFileExists))
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
